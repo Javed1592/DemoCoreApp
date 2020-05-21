9bcf6e8461f6abd79b50d250c989526bc650df95
Shaik <Javedbasha.Shaik@CenturyLink.com>
2020-05-21 22:09:44 +0530

    Add project files.

---
+++ DemoCoreApp/Context/EmployeeContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DemoCoreApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DemoCoreApp.Context
+{
+    public class EmployeeContext : DbContext
+    {
+        public DbSet<Employee> Employee { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server = tcp:ctlserver.database.windows.net, 1433; Initial Catalog = db_test; Persist Security Info = False; User ID = shaik1592; Password =Sh@ik123; MultipleActiveResultSets = False; Encrypt = True; TrustServerCertificate = False; Connection Timeout = 30;");
+        }
+
+
+    }
+}
---
+++ DemoCoreApp/Models/Employee.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DemoCoreApp.Models
+{
+    public class Employee
+    {
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int employeeId { get; set; }
+        [Required]
+        [Column(TypeName = "varchar(30)")]
+        public string firstName { get; set; }
+
+        [Required]
+        [Column(TypeName = "varchar(30)")]
+        public string lastName { get; set; }
+
+        [Required]
+        [Column(TypeName = "varchar(20)")]
+        public string gender { get; set; }
+
+        [Required]
+        [Column(TypeName = "varchar(30)")]
+        public string designation { get; set; }
+
+        [Required]
+        [Column(TypeName = "varchar(40)")]
+        public string department { get; set; }
+
+    }
+}
